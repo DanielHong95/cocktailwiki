711f3dcfa5051ff18e110f09ee8759138e8e8564
DanielHong95 <danielhong95@outlook.com>
2022-07-22 16:03:22 -0500

    gitignore and env password

--- server/db.js
+++ server/db.js
@@ -2,7 +2,7 @@ const Pool = require("pg").Pool;
 
 const pool = new Pool({
   user: "postgres",
-  password: "hd.214375",
+  password: process.env.DATABASE_PASSWORD,
   host: "localhost",
   port: 5432,
   database: "cocktaildb",
