1dc6069f41e65079e47f915947b8fec4b94d6528
DanielHong95 <danielhong95@outlook.com>
2022-08-12 14:52:34 -0500

    .env

--- server/db.js
+++ server/db.js
@@ -1,8 +1,9 @@
 const Pool = require("pg").Pool;
+require("dotenv").config();
 
 const pool = new Pool({
   user: "postgres",
-  password: "hd.214375",
+  password: process.env.DATABASE_PASSWORD,
   host: "localhost",
   port: 5432,
   database: "cocktaildb",
