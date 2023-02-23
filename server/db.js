6d631c8b989513f656c3d7934bca6128c31d5937
Sean Dan <seanysdan@gmail.com>
2023-02-23 13:53:54 -0600

    fix env vars

--- server/db.js
+++ server/db.js
@@ -2,11 +2,11 @@ const Pool = require("pg").Pool;
 require("dotenv").config();
 
 const pool = new Pool({
-  user: "postgres",
+  user: process.env.PGUSER,
   password: process.env.DATABASE_PASSWORD,
   host: process.env.PGHOST || "localhost",
   port: 5432,
-  database: "cocktaildb",
+  database: process.env.PGDATABASE,
 });
 
 module.exports = pool;
